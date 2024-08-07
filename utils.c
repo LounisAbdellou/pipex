5bfce3e7f4a269092b7e6d107ba53060c11e4ada
LounisAbdellou <lounisabdellou.dev@gmail.com>
2024-08-07 10:52:31 +0200

    ok

--- utils.c
+++ utils.c
@@ -1,38 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: labdello <labdello@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/03 18:01:50 by labdello          #+#    #+#             */
-/*   Updated: 2024/08/07 09:45:21 by labdello         ###   ########.fr       */
+/*   Created: 2024/08/07 09:45:38 by labdello          #+#    #+#             */
+/*   Updated: 2024/08/07 09:45:53 by labdello         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	pipex(char **args, int infile, int outfile, char **env)
-{
-	int	i;
-
-	i = 0;
-	while (args[i + 1] != NULL)
-	{
-		i++;
-	}
-}
-
-int	main(int ac, char **av, char **env)
-{
-	int	infile;
-	int	outfile;
-
-	if (ac < 5)
-		return (1);
-	infile = handle_open(av[1], 0);
-	outfile = handle_open(av[ac - 1], 1);
-	pipex(av + 2, infile, outfile, env);
-	(void)av;
-	return (0);
-}
