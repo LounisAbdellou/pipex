5fbe941f8f10aa590ac439e34307a780ddd9447c
Lounis Abdellou <lounisabdellou.dev@gmail.com>
2024-08-12 18:12:27 +0200

    ok

--- main.c
+++ main.c
@@ -6,28 +6,37 @@
 /*   By: labdello <labdello@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 18:01:50 by labdello          #+#    #+#             */
-/*   Updated: 2024/08/09 18:32:58 by labdello         ###   ########.fr       */
+/*   Updated: 2024/08/12 17:41:18 by labdello         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	exec(char *cmd, char **env);
+void	exec(char *cmd, char **env)
+{
+
+}
 
 void	open_pipe(char *cmd, char **env);
 {
 	int	pid;
 	int	pipe_fd[2];
 
+	if (pipe(pipe_fd) == -1)
+		exit(0);
 	pid = fork();
-	pipe(pipe_fd);
-	if (pid < 0)
+	if (pid == -1)
+		exit(0);
+	if (pid == 0)
 	{
-
+		close(pipe_fd[0]);
+		dup2(pipe_fd[1], 1);
+		exec(cmd, env);
 	}
 	else
 	{
-
+		close(pipe_fd[1]);
+		dup2(pipe_fd[0], 0);
 	}
 	close(pipe_fd[0]);
 	close(pipe_fd[1]);
@@ -39,10 +48,13 @@ void	pipex(char **args, int infile, int outfile, char **env)
 	int	i;
 
 	i = 0;
+	dup2(infile, 0);
 	while (args[i + 1] != NULL)
 	{
+		open_pipe(args[i], env);
 		i++;
 	}
+	dup2(outfile, 1);
 }
 
 int	main(int ac, char **av, char **env)
@@ -55,6 +67,5 @@ int	main(int ac, char **av, char **env)
 	infile = handle_open(av[1], 0);
 	outfile = handle_open(av[ac - 1], 1);
 	pipex(av + 2, infile, outfile, env);
-	(void)av;
 	return (0);
 }
